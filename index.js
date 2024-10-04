4e5af311fd1c3f9465269b6a41812eb5ead507b0
rabiul3000 <mdrabiul.asia@gmail.com>
2024-10-04 19:51:34 +0600

    modified index file

--- index.js
+++ index.js
@@ -7,67 +7,67 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-	session({
-		secret: process.env.TWITTER_API_SECRET,
-		resave: false,
-		saveUninitialized: true,
-	})
-);
+// app.use(
+// 	session({
+// 		secret: process.env.TWITTER_API_SECRET,
+// 		resave: false,
+// 		saveUninitialized: true,
+// 	})
+// );
 
-const twitterClient = new Twitter({
-	consumer_key: process.env.TWITTER_API_KEY,
-	consumer_secret: process.env.TWITTER_API_SECRET,
-});
+// const twitterClient = new Twitter({
+// 	consumer_key: process.env.TWITTER_API_KEY,
+// 	consumer_secret: process.env.TWITTER_API_SECRET,
+// });
 
-app.get('/auth/twitter', async (req, res) => {
-	try {
-		const response = await twitterClient.getRequestToken(
-			'http://localhost:3000/auth/twitter/callback'
-		);
+// app.get('/auth/twitter', async (req, res) => {
+// 	try {
+// 		const response = await twitterClient.getRequestToken(
+// 			'http://localhost:3000/auth/twitter/callback'
+// 		);
 
-		// Store token and token secret in session
-		req.session.oauth_token = response.oauth_token;
-		req.session.oauth_token_secret = response.oauth_token_secret;
+// 		// Store token and token secret in session
+// 		req.session.oauth_token = response.oauth_token;
+// 		req.session.oauth_token_secret = response.oauth_token_secret;
 
-		// Redirect user to Twitter to authenticate
-		res.redirect(
-			`https://api.twitter.com/oauth/authenticate?oauth_token=${response.oauth_token}`
-		);
-	} catch (error) {
-		console.error('Error getting request token', error);
-		res.status(500).send('Error getting request token');
-	}
-});
+// 		// Redirect user to Twitter to authenticate
+// 		res.redirect(
+// 			`https://api.twitter.com/oauth/authenticate?oauth_token=${response.oauth_token}`
+// 		);
+// 	} catch (error) {
+// 		console.error('Error getting request token', error);
+// 		res.status(500).send('Error getting request token');
+// 	}
+// });
 
-app.get('auth/twitter/callback', async (req, res) => {
-	const { oauth_token, oauth_verifier } = req.query;
+// app.get('auth/twitter/callback', async (req, res) => {
+// 	const { oauth_token, oauth_verifier } = req.query;
 
-	if (oauth_token !== req.session.oauth_token) {
-		return res.status(400).send('OAuth token mismatch');
-	}
+// 	if (oauth_token !== req.session.oauth_token) {
+// 		return res.status(400).send('OAuth token mismatch');
+// 	}
 
-	try {
-		// Use the stored oauth_token and secret to get the access token
-		const response = await twitterClient.getAccessToken({
-			oauth_token: req.session.oauth_token,
-			oauth_token_secret: req.session.oauth_token_secret,
-			oauth_verifier,
-		});
+// 	try {
+// 		// Use the stored oauth_token and secret to get the access token
+// 		const response = await twitterClient.getAccessToken({
+// 			oauth_token: req.session.oauth_token,
+// 			oauth_token_secret: req.session.oauth_token_secret,
+// 			oauth_verifier,
+// 		});
 
-		// Store the access token and secret for future API requests
-		req.session.access_token = response.oauth_token;
-		req.session.access_token_secret = response.oauth_token_secret;
-		req.session.user_id = response.user_id;
-		req.session.screen_name = response.screen_name;
+// 		// Store the access token and secret for future API requests
+// 		req.session.access_token = response.oauth_token;
+// 		req.session.access_token_secret = response.oauth_token_secret;
+// 		req.session.user_id = response.user_id;
+// 		req.session.screen_name = response.screen_name;
 
-		// Redirect to dashboard after login
-		res.redirect('/dashboard');
-	} catch (error) {
-		console.error('Error getting access token', error);
-		res.status(500).send('Error getting access token');
-	}
-});
+// 		// Redirect to dashboard after login
+// 		res.redirect('/dashboard');
+// 	} catch (error) {
+// 		console.error('Error getting access token', error);
+// 		res.status(500).send('Error getting access token');
+// 	}
+// });
 
 
 
